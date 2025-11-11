111583966f19e316a4d12414fd39de2ecb9fa0f0
Blackott1221 <js.schwarzott@icloud.com>
2025-11-11 08:42:09 +0700

    fix: Remove missing EarlyAccessBanner component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { EarlyAccessBanner } from "@/components/ui/early-access-banner";
 import { useAuth } from "@/hooks/useAuth";
 import NotFound from "@/pages/not-found";
 import Landing from "@/pages/landing";
@@ -105,16 +104,8 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <div className="dark aras-bg-animated">
-          {/* Early Access Banner - Ganz oben fixiert */}
-          <div className="fixed top-0 left-0 right-0 z-[9999]">
-            <EarlyAccessBanner />
-          </div>
-          
-          {/* Main Content - mit Padding-Top für Banner */}
-          <div className="pt-8">
-            <Toaster />
-            <Router />
-          </div>
+          <Toaster />
+          <Router />
         </div>
       </TooltipProvider>
     </QueryClientProvider>
